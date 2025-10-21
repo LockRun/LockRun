f8a9614b7ebaca6e3c1c66b34fda3c66577035a1
leesanghyuk <leetkdgurdl@naver.com>
2025-10-21 14:32:24 +0900

    [Android/Setting] gitignore 설정

--- Android/LockRunApp/app/build.gradle.kts
+++ Android/LockRunApp/app/build.gradle.kts
@@ -16,6 +16,8 @@ android {
 }
 dependencies {
 
+    implementation(project(":features:home"))
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
