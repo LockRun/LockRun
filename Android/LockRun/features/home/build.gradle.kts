a7649204941fabfa55791b538af53417fe074c4f
leesanghyuk <leetkdgurdl@naver.com>
2025-10-23 23:25:11 +0900

    [Android/Feat] 네비게이션 설정
    - FragmentContainerView과 navigation을 사용하여 연결시킴

--- Android/LockRun/features/home/build.gradle.kts
+++ Android/LockRun/features/home/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.tteoli.android.library")
     id("com.tteoli.android.compose")
+    alias(libs.plugins.kotlin.android)
 }
 
 android {
@@ -11,11 +12,17 @@ android {
 dependencies {
 
     implementation(project(":core"))
-    
+
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.compose.ui)
+    implementation(libs.androidx.compose.ui.graphics)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
 }
